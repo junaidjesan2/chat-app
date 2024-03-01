7de5d858ce1885bd500e0ea7bdf38cea20812e86
junaidjesan2 <junaidjesan2@gmail.com>
2024-03-01 12:39:45 +0600

    header section has created

--- src/components/shared/Header.jsx
+++ src/components/shared/Header.jsx
@@ -1,7 +1,17 @@
-import React from 'react'
+import React from "react";
+import logo from "../../assets/images/Logo.png";
+import { Link } from "react-router-dom";
 
 export default function Header() {
   return (
-    <div>Header</div>
-  )
+    <div className="flex items-center justify-around px-5 py-7">
+      <div>
+        <img src={logo} alt="" />
+      </div>
+      <div className="flex gap-6 items-center">
+        <Link to="/">Home</Link>
+        <Link to="/">Exercises</Link>
+      </div>
+    </div>
+  );
 }
